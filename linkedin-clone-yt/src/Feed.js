da6d82ce0a81b6428214592a59063a1a3bf2902e
Felipe Campanhol Manoel de Jesus <fcampanhol@hotmail.com>
2022-04-17 01:26:10 -0300

    Creating page for linkedIn clone with login

--- linkedin-clone-yt/src/Feed.js
+++ linkedin-clone-yt/src/Feed.js
@@ -1,23 +1,72 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Feed.css'
-import './InputOption'
+import InputOption from './InputOption'
 import CreateIcon from "@mui/icons-material/Create"
+import ImageIcon from "@mui/icons-material/Image"
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions"
+import EventNoteIcon from "@mui/icons-material/EventNote"
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay"
+import Post from './Post'
+import { db } from './firebase'
+import { serverTimestamp, collection, onSnapshot, setDoc, doc, orderBy, query } from "firebase/firestore";
 
 function Feed() {
+
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(query(collection(db, "posts"), orderBy("timestamp", "desc")),(snapshot) => {
+        setPosts(
+            snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            }))
+        )})
+}, []);
+
+  const sendPost = (e) => {
+      e.preventDefault();
+      
+      setDoc(doc(collection(db, "posts")), {
+        name: "Felipe Campanhol",
+        description: "this is a test",
+        message: input,
+        photoUrl: "",
+        timestamp: serverTimestamp(),
+      });
+      setInput("")
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
           <div className="feed__input">
               <CreateIcon />
-              <form action="">
-                  <input type="text" />
-                  <button type="submit">Send</button>
+              <form>
+                  <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+                  <button onClick={sendPost} type="submit">Send</button>
               </form>
           </div>
           <div className="feed__inputOptions">
-              <InputOption />
+              <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+              <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+              <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+              <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
           </div>
       </div>
+
+      {/* Posts */}
+        {posts.map(({id, data: {name, description, message, photoUrl}}) => (
+            <Post
+                key={id}
+                name={name}
+                description={description}
+                message={message}
+                photoUrl={photoUrl}
+            />
+        ))}
+
     </div>
   );
 }
---
+++ linkedin-clone-yt/src/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { login } from './features/userSlice';
+import { useDispatch } from "react-redux"
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth"
+import './Login.css'
+
+function Login() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [profilePic, setProfilePic] = useState("")
+    const dispatch = useDispatch();
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+
+    const register = () => {
+        if(!name){
+            return alert("Please enter a full name!")
+        }
+
+        createUserWithEmailAndPassword(getAuth(), email, password)
+        .then((userAuth) => {
+            updateProfile( getAuth().currentUser,{
+                displayName: name,
+                photoURL: profilePic,
+            })
+            .then(() => {
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoURL: profilePic,
+                }))
+            })
+        }).catch(error => alert(error));
+    };
+
+  return (
+    <div className="login">
+      <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="" />
+
+      <form>
+        <input value={name} onChange={(e) => setName(e.target.value)}  placeholder="Full name (required if registered)" type="text" />
+
+        <input value={profilePic} onChange={(e) => setProfilePic(e.target.value)} placeholder="Profile pic URL (optional)" type="text" />
+
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" />
+
+        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+
+        <button type="submit" onClick={loginToApp}>Sign In</button>
+      </form>
+      <p>Not a member?{" "}
+          <span className="login__register" onClick={register}>Register now</span>
+      </p>
+    </div>
+  )
+}
+
+export default Login;
