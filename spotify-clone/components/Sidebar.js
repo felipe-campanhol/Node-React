e663b423c800e9995f89939529bef2265af7c5ef
Felipe Campanhol Manoel de Jesus <fcampanhol@hotmail.com>
2022-04-26 21:49:21 -0300

    Starting spotify project

---
+++ spotify-clone/components/Sidebar.js
@@ -0,0 +1,20 @@
+import {
+    HomeIcon,
+    SearchIcon,
+    LibraryIcon,
+    PlusCircleIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div>
+      <div>
+          <button>
+              <HomeIcon />
+          </button>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
